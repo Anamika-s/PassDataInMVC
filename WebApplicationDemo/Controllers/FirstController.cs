fa7bbe27593aee2371b93b15251c5c23913cd7d4
anamikasawhney <anamika.sawhney22@gmail.com>
2021-06-08 16:40:45 +0530

    Add project files.

---
+++ WebApplicationDemo/Controllers/FirstController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplicationDemo.Controllers
+{
+    public class FirstController : Controller
+    {
+
+       
+        public ActionResult Index()
+        {
+            ViewBag.data = "hello";
+            ViewBag.Date = DateTime.Now.ToString();
+            return View();
+        }
+
+        public ActionResult Method1()
+        {
+            if(TempData["CompanyName"] !=null)
+            ViewBag.CompanyName = TempData["CompanyName"];
+            return View();
+        }
+
+        public ActionResult Method2()
+        {
+            ViewBag.name = "Ajay Sood";
+            ViewData["address"] = "Delhi";
+            return View();
+        }
+
+
+        public string Method3()
+        {
+            return "Hello";
+        }
+
+        public ActionResult Method4()
+        {
+            return RedirectToAction("Method1");
+        }
+
+        public ActionResult Method5()
+        {
+            TempData["CompanyName"] = "CTS";
+            return RedirectToAction("Method1");
+        }
+
+
+    }
+}
